d07254d8ff8b6bdb8b280127613c13b4f9b07783
dhui <dhui0226@gmail.com>
2021-07-21 23:51:56 -0500

    message change

--- index.js
+++ index.js
@@ -7,12 +7,23 @@ client.once('ready', () => {
     console.log(`logged in as ${client.user.tag}`)
 })
 
-client.on('guildMemberAdd', member => {
+client.on('guildMemberAdd', async member => {
     const channel = member.guild.channels.cache.find(ch => ch.name === 'general')
 
     if (!channel) return
 
-    channel.send(`Welcome to ${message.guild.name}, ${member}! To look up the price of a cryptocurrency, just type !anyCoinName. For example, !bitcoin`)
+    channel.send(`Welcome to ${message.guild.name}, ${member}! 
+        To look up the price of a cryptocurrency, just type !anyCoinName. 
+        For example, !bitcoin. Here is a list of 10 coins you might be interested in.`)
+
+    const coins = await getCoins()
+        
+    coins.map((coin) => {
+        if (coin.market_data.market_cap_rank <= 10) {
+            message.channel.send(coin.id)
+        }
+    })
+    client.emit("guildMemberAdd", member)
 })
 
 client.on('message', async (message) => {
@@ -34,7 +45,7 @@ client.on('message', async (message) => {
         const queriedCoin = message.content.slice(1)
 
         try {
-            const coin = await getCoin(queriedCoin)
+            const coin = await getCoin(queriedCoin.toLowerCase())
             const name = coin.id 
             const price = coin.market_data.current_price.usd
         
