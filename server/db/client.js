afe0df1b19b282d77174662388c781320ff9220c
dhui <dhui0226@gmail.com>
2021-07-27 18:05:20 -0500

    client.js

--- server/db/client.js
+++ server/db/client.js
@@ -1,5 +1,8 @@
 const { Client } = require('pg')
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/coinList'
-const client = new Client(connectionString)
+const client = new Client({
+    connectionString,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined
+})
 
 module.exports = { client }
