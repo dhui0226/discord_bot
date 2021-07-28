c62a0e58fb4453a1307019652142333752ddd8d9
dhui <dhui0226@gmail.com>
2021-07-28 01:02:31 -0500

    changed ssl

--- server/db/client.js
+++ server/db/client.js
@@ -2,7 +2,7 @@ const { Client } = require('pg')
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/coinList'
 const client = new Client({
     connectionString,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined
+    ssl: { rejectUnauthorized: false }
 })
 
 module.exports = { client }
